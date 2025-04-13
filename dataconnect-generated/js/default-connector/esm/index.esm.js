f04b78c0ff06a2abcb54d2481b954420ebc830c9
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2025-04-13 18:16:30 +0530

    dkjsaflajks

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'awesome-9ddc4',
-  location: 'asia-east1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,8 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'awesome-9ddc4',
-  location: 'asia-east1'
-};
-exports.connectorConfig = connectorConfig;
-
