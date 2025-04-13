aac6b98753912225b53d668d0edc883831697a6a
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2025-04-13 16:13:01 +0530

    jhfajkdh

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'awesome-9ddc4',
+  location: 'asia-east1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'awesome-9ddc4',
+  location: 'asia-east1'
+};
+exports.connectorConfig = connectorConfig;
+
